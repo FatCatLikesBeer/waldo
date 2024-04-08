c1234ec57a07f3359686a5224de3fd1dd83d93fa
Billy Bagel <itisbilly@gmail.com>
2024-04-08 16:23:54 -0700

    Added subhead & moved the click modal around a bit

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -3,6 +3,7 @@ export default function Top() {
   return(
     <div id="navbar">
       <h1 id="navtitle"><a href="/">Find Stuff</a></h1>
+      <h3 id="subtitle">A Where's Waldo Type Game</h3>
       <div id="navlinksdiv">
         <ul id="navlinks">
           <li>
